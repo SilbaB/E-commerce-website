db7cbf4973a91eb15d79469b76fadeb533b6976f
SilbaB <murkomenbasil@gmail.com>
2023-10-16 18:57:18 +0300

    FAQ page section added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+const accordion= document.getElementsByClassName('container');
 
+// accordion.forEach(item, ()=>{
+//     item.addEventListener('click',()=>{
+//         item.classList.toggle('active');
+//    })
+// })
+for(let i=0;i<accordion.length;i++){
+    accordion[i].addEventListener('click',function (){
+        this.classList.toggle('active')
+    })
+}
