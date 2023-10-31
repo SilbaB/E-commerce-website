52e18dcbe6fbb13a3c755d930c6fbe5e924e4e3d
SilbaB <murkomenbasil@gmail.com>
2023-10-31 09:52:13 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const accordion= document.getElementsByClassName('container');
 
 const hamburgerMenu=document.querySelector('.menuBar');
 const myList=document.querySelector('.nav-list');
+console.log(myList);
 
 for(let i=0;i<accordion.length;i++){
     accordion[i].addEventListener('click',function (){
